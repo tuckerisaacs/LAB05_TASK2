293e877374408e658537e99ac7771f411090910b
tucker isaacs <isaacstb@mail.uc.edu>
2022-09-22 15:02:02 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int birthMonth = 13;
+
+        if (birthMonth >= 1 && birthMonth <= 12) {
+            System.out.println("your birthmonth is " + birthMonth);
+        } else
+            System.out.println("invalid birth month");
+
+
+
+    }
+}
